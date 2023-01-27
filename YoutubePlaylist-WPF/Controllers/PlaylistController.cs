04f212cb9c98e9aa1093213e1f8b8ccfa97ea440
Corey <Corey255A1@hotmail.com>
2023-01-27 09:23:16 -0500

    Beginnings of the playlist

---
+++ YoutubePlaylist-WPF/Controllers/PlaylistController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubePlaylistWPF.Data;
+
+namespace YoutubePlaylistWPF.Controllers
+{
+    public class PlaylistController: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void Notify([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ObservableCollection<PlaylistItem> Playlist { get; set; }
+
+        private PlaylistItem? _currentPlaylistItem;
+        public PlaylistItem? CurrentPlaylistItem
+        {
+            get { return _currentPlaylistItem; }
+            set {
+                _currentPlaylistItem = value;
+                Notify();
+            }
+        }
+
+
+
+        public PlaylistController() {
+            Playlist = new ObservableCollection<PlaylistItem>()
+            {
+                new PlaylistItem()
+                {
+                    URL="KFBgEsjaJoc",
+                    ID=1
+                },
+                new PlaylistItem()
+                {
+                    URL="jX21YCADuTA",
+                    ID=2
+                },
+                new PlaylistItem()
+                {
+                    URL="oC3DuSahiZc",
+                    ID=3
+                }
+            };
+        }
+
+    }
+}
---
+++ YoutubePlaylist-WPF/Data/PlaylistItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubePlaylistWPF.Data
+{
+    public class PlaylistItem
+    {
+        public string URL { get; set; } = "";
+        public int ID { get; set; } = 0;
+    }
+}
