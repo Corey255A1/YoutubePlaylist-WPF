1efdc3982134455f6dd6dd1f88f7b72998b0784e
Corey <Corey255A1@hotmail.com>
2023-01-29 09:47:23 -0500

    Connecting up the Add/Remove and Next/Previous commands

--- YoutubePlaylist-WPF/Controllers/PlaylistController.cs
+++ YoutubePlaylist-WPF/Controllers/PlaylistController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using YoutubePlaylistWPF.Data;
 
 namespace YoutubePlaylistWPF.Controllers
@@ -27,48 +29,99 @@ namespace YoutubePlaylistWPF.Controllers
         }
 
 
-        public PlaylistCommand PlayItemCommand { get; set; }
-        public PlaylistCommand RemoveItemCommand { get; set; }
-        public PlaylistCommand MoveItemUpCommand { get; set; }
-        public PlaylistCommand MoveItemDownCommand { get; set; }
+        public ICommand PlayItemCommand { get; set; }
+        public ICommand PlayNextItemCommand { get; set; }
+        public ICommand PlayPreviousItemCommand { get; set; }
+        public ICommand RemoveItemCommand { get; set; }
+        public ICommand MoveItemUpCommand { get; set; }
+        public ICommand MoveItemDownCommand { get; set; }
+        public ICommand AddItemToTopCommand { get; set; }
+        public ICommand AddItemToBottomCommand { get; set; }
 
         public PlaylistController()
         {
-            PlayItemCommand = new PlaylistCommand(PlayItem);
-            RemoveItemCommand = new PlaylistCommand(RemoveItem);
-            MoveItemUpCommand = new PlaylistCommand(MoveItemUp);
-            MoveItemDownCommand = new PlaylistCommand(MoveItemDown);
+            PlayItemCommand = new PlaylistItemCommand(PlayItem);
+            
+            RemoveItemCommand = new PlaylistItemCommand(RemoveItem);
+            MoveItemUpCommand = new PlaylistItemCommand(MoveItemUp);
+            MoveItemDownCommand = new PlaylistItemCommand(MoveItemDown);
+
+            AddItemToTopCommand = new PlaylistControlCommand(AddItemToTop);
+            AddItemToBottomCommand = new PlaylistControlCommand(AddItemToBottom);
+
+            PlayNextItemCommand = new PlaylistControlCommand(PlayNextItem);
+            PlayPreviousItemCommand = new PlaylistControlCommand(PlayPreviousItem);
 
             Playlist = new ObservableCollection<PlaylistItem>()
             {
                 new PlaylistItem()
                 {
                     URL="KFBgEsjaJoc",
-                    ID=1
+                    ID="1"
                 },
                 new PlaylistItem()
                 {
                     URL="jX21YCADuTA",
-                    ID=2
+                    ID="2"
                 },
                 new PlaylistItem()
                 {
                     URL="oC3DuSahiZc",
-                    ID=3
+                    ID="3"
                 }
             };
 
             CurrentPlaylistItem = Playlist[1];
         }
 
+        public void AddItemToTop()
+        {
+            var item = new PlaylistItem() { ID = Guid.NewGuid().ToString() };
+            Playlist.Insert(0, item);
+        }
+
+        public void AddItemToBottom()
+        {
+            var item = new PlaylistItem() { ID = Guid.NewGuid().ToString() };
+            Playlist.Add(item);
+        }
+
         public void PlayItem(PlaylistItem item)
         {
             CurrentPlaylistItem = item;
         }
+
+        public void PlayNextItem()
+        {
+            int itemIndex = -1;
+            if (CurrentPlaylistItem != null)
+            {
+                itemIndex = Playlist.IndexOf(CurrentPlaylistItem);
+            }
+            
+            if ((itemIndex + 1) >= Playlist.Count) { return; }
+
+            PlayItem(Playlist[itemIndex + 1]);
+        }
+
+        public void PlayPreviousItem()
+        {
+            int itemIndex = Playlist.Count;
+            if (CurrentPlaylistItem != null)
+            {
+                itemIndex = Playlist.IndexOf(CurrentPlaylistItem);
+            }
+
+            if ((itemIndex - 1) < 0) { return; }
+
+            PlayItem(Playlist[itemIndex - 1]);
+        }
+
         public void RemoveItem(PlaylistItem item)
         {
             Playlist.Remove(item);
         }
+
         public void MoveItemUp(PlaylistItem item)
         {
             int itemIndex = Playlist.IndexOf(item);
@@ -76,6 +129,7 @@ namespace YoutubePlaylistWPF.Controllers
 
             Playlist.Move(itemIndex, itemIndex - 1);
         }
+
         public void MoveItemDown(PlaylistItem item)
         {
             int itemIndex = Playlist.IndexOf(item);
--- YoutubePlaylist-WPF/Data/PlaylistItem.cs
+++ YoutubePlaylist-WPF/Data/PlaylistItem.cs
@@ -3,6 +3,6 @@
     public class PlaylistItem
     {
         public string URL { get; set; } = "";
-        public int ID { get; set; } = 0;
+        public string ID { get; set; } = "0";
     }
 }
