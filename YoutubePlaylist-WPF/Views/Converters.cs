b3e41e7e6b84811627138c61cbd2a4f015b7cfd3
Corey <Corey255A1@hotmail.com>
2023-01-29 09:52:28 -0500

    Breaking out the converter

---
+++ YoutubePlaylist-WPF/Views/Converters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace YoutubePlaylistWPF.Views
+{
+    public class SelectedItemConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] == values[1])
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+            return new SolidColorBrush(Colors.AliceBlue);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- YoutubePlaylist-WPF/Views/Playlist.xaml.cs
+++ YoutubePlaylist-WPF/Views/Playlist.xaml.cs
@@ -16,21 +16,4 @@ namespace YoutubePlaylistWPF.Views
             InitializeComponent();
         }
     }
-
-    public class SelectedItemConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (values[0] == values[1])
-            {
-                return new SolidColorBrush(Colors.Green);
-            }
-            return new SolidColorBrush(Colors.AliceBlue);
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
