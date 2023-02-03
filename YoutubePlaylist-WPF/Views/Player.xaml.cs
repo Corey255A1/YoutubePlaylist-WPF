fae36e53a90fbe28c0507bf7fa7ef576351dcee4
Corey <Corey255A1@hotmail.com>
2023-02-02 22:15:41 -0500

    Trying out the basics of the Iframe controller Play Pause functional!

--- YoutubePlaylist-WPF/Views/Player.xaml.cs
+++ YoutubePlaylist-WPF/Views/Player.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace YoutubePlaylistWPF.Views
@@ -8,6 +10,85 @@ namespace YoutubePlaylistWPF.Views
     /// </summary>
     public partial class Player : UserControl
     {
+
+
+        public string YoutubeTitle
+        {
+            get { return (string)GetValue(YoutubeTitleProperty); }
+            set { SetValue(YoutubeTitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for YoutubeTitle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty YoutubeTitleProperty =
+            DependencyProperty.Register("YoutubeTitle", typeof(string), typeof(Player), new PropertyMetadata(""));
+
+
+
+        private static readonly string TESTIFRAME =
+@"
+<html>
+
+<head>
+    <style>
+        html,
+        body {
+            background: lightgray;
+            overflow: hidden;
+            padding: 0;
+            margin: 0;
+        }
+
+        iframe {
+            border: none;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <iframe width="" 300"" height="" 300"" src=""https://www.youtube.com/embed/KFBgEsjaJoc?enablejsapi=1&mute=1""
+        allow="" accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share""
+        allowFullScreen id=""wpf-player""></iframe>
+    <script>
+        let youtubePlayer = null;
+        function playerStateChangeHandler(e) {
+            chrome.webview.postMessage(`${e.data}:${e.target.videoTitle}`);
+
+        }
+        function play() {
+            if (youtubePlayer != null) { youtubePlayer.playVideo(); }
+        }
+        function pause() {
+            if (youtubePlayer != null) { youtubePlayer.pauseVideo(); }
+        }
+
+        function loadVideoById(videoId, startSeconds) {
+            if (youtubePlayer != null) { youtubePlayer.loadVideoById(videoId, startSeconds); }
+        }
+
+        const tag = document.createElement('script');
+        tag.src = 'https://www.youtube.com/iframe_api';
+
+        // onYouTubeIframeAPIReady will load the video after the script is loaded
+        window.onYouTubeIframeAPIReady = () => {
+            const playerFrame = new window.YT.Player('wpf-player',
+                {
+                    events: {
+                        'onReady': (event) => { youtubePlayer = event.target },
+                        'onStateChange': playerStateChangeHandler
+                    }
+                });
+
+        }
+
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
+    </script>
+</body>
+
+</html>
+";
         public Player()
         {
             InitializeComponent();
@@ -17,7 +98,24 @@ namespace YoutubePlaylistWPF.Views
         private async void PlayerLoaded(object sender, System.Windows.RoutedEventArgs e)
         {
             await webView.EnsureCoreWebView2Async();
-            webView.NavigateToString("<html><head><style>html,body{background:lightgray;}</style><heaed><body><h1>Hello World</h1></body></html>");
+            webView.NavigateToString(TESTIFRAME);
+            webView.CoreWebView2.WebMessageReceived += YoutubeWebviewWebMessageReceived;
+        }
+
+        private void YoutubeWebviewWebMessageReceived(object? sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            string[] response = e.WebMessageAsJson.Trim('"').Split(':', 2);
+            YoutubeTitle = response[1];
+        }
+
+        private void PlayClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            webView.ExecuteScriptAsync("play()");
+        }
+
+        private void PauseClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            webView.ExecuteScriptAsync("pause()");
         }
     }
 }
